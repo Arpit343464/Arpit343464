d2e61db60d9564a1634a23eb86bd556cc5d389b1
Arpit343464 <101986213+Arpit343464@users.noreply.github.com>
2022-10-24 19:38:21 +0530

    Update Q2_Quadratic_Probing.java

--- Lab_6/Q2_Quadratic_Probing.java
+++ Lab_6/Q2_Quadratic_Probing.java
@@ -8,28 +8,51 @@ class Quadratic_Probing{
     int M;
     int[] hashArray;
     int[] keys;
+    int home;
 
     public Quadratic_Probing(int M, int[] keys){
         this.M = M;
         this.keys = keys;
         this.hashArray = new int[M];
-        for (int key: keys) {
-            int home = key % M;
-            int index = home;
-            int flag = 0;
-            int count = 1;
-            while(hashArray[index] != 0){
-                int place = index + count*count;
-                if(place >= M){
-                    index++;
-                }
 
-            }
-            if(flag == 1){
+        for (int key: keys) {
+            this.home = key % M;
+            add(home, key, hashArray);
+        }
+    }
+
+    public void add(int place, int key, int[] hashArray){
+        int i = 0;
+        int count = 0;
+        int flag = 0;
+        int index = place + i*i;
+        while(hashArray[index] != 0){
+            i++;
+            index = place + i*i;
+            if(index >= place && count > 0){
+                place = place + 1;
+                if(place == M){
+                    place = 0;
+                }
+                add(place, key, hashArray);
+                flag = 1;
                 break;
             }
+            if(index >= M) {
+                index = index - M;
+                count++;
+            }
+            if(place == home - 1){
+                M = M*2;
+                Quadratic_Probing newHash = new Quadratic_Probing(M, keys);
+                setHashArray(newHash.hashArray);
+                break;
+            }
+        }
+        if(flag == 0){
             hashArray[index] = key;
         }
+
     }
 
     public void setHashArray(int[] hashArray){
@@ -45,30 +68,41 @@ class Quadratic_Probing{
     }
 
     public int search(int key){
-        int home = key % M;
-        int index = home;
-        int i = 1;
-        int count = 0;
-        while(keys[index] != 0){
-            index = index + i*i;
-            i++;
+        home = key % M;
+        int index = inPlace(home, key, hashArray);
+        return index;
+    }
 
-            if(index > M && count > 0){
-                home ++;
-            }
-            if(index > M){
+    public int inPlace(int place, int key, int[] hashArray){
+        int i = 0;
+        int count = 0;
+        int index = place + i*i;
+        while(hashArray[index] != key){
+            i++;
+            index = place + i*i;
+            if(index >= M){
                 index = index - M;
                 count++;
             }
+            if(index >= place && count > 0){
+                place = place + 1;
+                if(place == M){
+                    place = 0;
+                }
+                return inPlace(place, key, hashArray);
+            }
+            if(place == home - 1){
+                return -1;
+            }
         }
-        return 0;
+        return index;
     }
 }
 public class Q2_Quadratic_Probing {
     public static void main(String[] args) {
         int[] keys = {133, 88, 92, 221, 174};
-        Quadratic_Probing M1 = new Quadratic_Probing(3, keys);
-        Quadratic_Probing M2 = new Quadratic_Probing(8, keys);
+        Quadratic_Probing M1 = new Quadratic_Probing(17, keys);
+        Quadratic_Probing M2 = new Quadratic_Probing(37, keys);
         M1.print();
         M2.print();
         Scanner sc = new Scanner(System.in);
