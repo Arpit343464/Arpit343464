e4218c8b3f7a3a479f995e435953de2d258d6548
Arpit343464 <101986213+Arpit343464@users.noreply.github.com>
2022-10-25 15:46:48 +0530

    Add files via upload

---
+++ Lab_6/Q5_polynomialHash.java
@@ -0,0 +1,52 @@
+package com.Lab.Lab_6;
+import java.util.Scanner;
+import java.lang.Math;
+import java.util.Arrays;
+class polynomialHash{
+     int M;
+     int a;
+     int[] hashArray;
+     String[] strKeys;
+     int[] keys;
+     Liner_Probing m;
+     public polynomialHash(int M, int a, String[] strKeys){
+         this.M = M;
+         this.a = a;
+         this.strKeys = strKeys;
+         this.hashArray = new int[M];
+         this.keys = new int[strKeys.length];
+         for (int i = 0; i < strKeys.length; i++) {
+             keys[i] = polynomial(strKeys[i], a);
+         }
+         this.m = new Liner_Probing(M, keys);
+     }
+     public void polynomialValue(){
+         System.out.println(Arrays.toString(keys));
+     }
+     public void print(){
+         m.print();
+     }
+     public int polynomial(String str, int a){
+         return value(str.length(), a, str);
+     }
+     public int value(int n, int a, String str){
+         int sum = 0;
+         for (int i = 0; i < n; i++) {
+             sum += str.charAt(i)*Math.pow(a, n - i);
+         }
+         return sum;
+     }
+}
+public class Q5_polynomialHash {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        String[] str = {"fist", "sift", "shift", "fast", "faster", "shaft"};
+        polynomialHash M1 = new polynomialHash(7, a, str);
+        polynomialHash M2 = new polynomialHash(8, a, str);
+        M1.polynomialValue();
+        M1.print();
+        M2.polynomialValue();
+        M2.print();
+    }
+}
